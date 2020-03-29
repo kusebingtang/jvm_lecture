e5aea6408086b3b3f7ff2844546e184b3dd7fb71
jbw <jb@98game.cn>
2020-03-29 16:31:10 +0800

    类加载器命名空间实战剖析与透彻理解

--- src/main/java/com/shengsiyuan/jvm/classloader/MyTest17Two.java
+++ src/main/java/com/shengsiyuan/jvm/classloader/MyTest17Two.java
@@ -3,6 +3,21 @@ package com.shengsiyuan.jvm.classloader;
 /**
  * @author JiangBin
  * @create 2020-03-29 16:01
+ *
+ * 1、由父加载器所加载的类是看不到子加载器所加载的类的，
+ * 2、由子加载器所加载的类是能看到父加载器所加载的类的
  */
 public class MyTest17Two {
+
+    public static void main(String[] args) throws Exception {
+
+        MyTest16 loader1 = new MyTest16("loader1");
+        loader1.setPath("/Users/zyh/Documents/JavaWorkSpace/jvm_lecture/bin/main/");
+        Class<?> mySampleClass = loader1.loadClass("com.shengsiyuan.jvm.classloader.MySample");
+        System.out.println("mySampleClass hashCode:" + mySampleClass.hashCode());
+        Object object = mySampleClass.newInstance();
+
+
+    }
+
 }
